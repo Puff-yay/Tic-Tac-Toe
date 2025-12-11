84ce362bffbb8bd65f3215594ce5e7cf72839503
Puff-yay <demonic.puff@gmail.com>
2025-12-11 12:01:18 -0500

    Code update
    Overhauled most of the code, made it more sleek, it can now run until draw, adding a win condition

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -1,51 +1,63 @@
 import time
 
 def starting_gameboard():
-    table = [[1,2,3], [4,5,6], [7,8,9]]
+    table = [[" "," "," "], [" "," "," "], [" "," "," "]]
     return table
-
-def position_converter_set_a(position):
-    column = position % 3
-    return column
-
-def position_converter_set_b(position):
-    row_num = (position // 3) - 1
-    return row_num
 
 def instructions():
     print("Let's play a game of tic tac toe.")
     time.sleep(2)
     print("Player 1 is always X and Player 2 is always O.")
     time.sleep(2)
-    print("Say the number you want your X or O to go in.")
+    print("The top left square is 1 and the bottom right square is 9.")
     time.sleep(2)
     print("Example: if you are playing X, its your turn, and the bottom left square is open, say 7")
 
 def tic_tac_toe():
     board = starting_gameboard()
+    player = True #True for X, False for Y
     player_num = 1
-    print(board[0])
-    print(board[1])
-    print(board[2])
-    [[0,0],[1,1],[2,2]]
-    # #1 set is upper left to bottom right diagonal.
-
+    turn = 0
     win = False
+    pos = 0
     while win != True:
-        position = input(f"You are player {player_num}. Where would you like to play?\n")
-        if board[position_converter_set_a(int(position))][position_converter_set_b(int(position))] not in ["X","Y"] and player_num == 1: #board[0][0] etc.
-            board[position_converter_set_a(int(position))][position_converter_set_b(int(position))] = "X"
-            player_num = 2
-        elif board[position_converter_set_a(int(position))][position_converter_set_b(int(position))] not in ["X","Y"] and player_num == 2:
-            board[position_converter_set_a(int(position))][position_converter_set_b(int(position))] = "Y"
-            player_num = 1
-        else:
-            print("That is not an open spot. Pick another one.")
+        print(board[0])
+        print(board[1])
+        print(board[2])
+        pos = input(f"You are player {player_num}. Where would you like to play?\n")
+        pos = int(pos) - 1
+        row = pos // 3
+        col = pos % 3
+        print("Calculating...")
+        time.sleep(0.5)
+        if pos < 0 or pos > 9:
+            print("Please pick another square")
+            continue
+        elif board[row][col] != " ":
+            print("This square is occupied!")
             time.sleep(1)
+            print("Please pick another square")
+        else:
+            if player:
+                board[row][col] = "X"
+                player = not player
+                player_num = 2
+                turn += 1
+            else:
+                board[row][col] = "Y"
+                player = not player
+                player_num = 1
+                turn += 1
+        if turn == 9:
+            print("This is a tied game.")
+            win = "Draw"
+    print("You have drawn this game. Thank you for playing.")
 
-#Need to add a while loop that tracks if the position of the board isn't in a winnning condition, continue the game.
-#Also need to add a condition where if a game is a tie, it proclaims a tie.
+def win_check():
+    winning_combos = [[0,0],[1,1],[2,2]],[[0,0],[1,0],[2,0]],[[0,0],[0,1],[0,2]],[[2,0],[2,1],[2,2]],[[1,0],[1,1],[1,2]],[[2,0],[1,1],[0,2]],[[0,1],[1,1],[2,1]],[[0,2],[1,2],[2,2]]
 
+
+#Make a win condition that loop through every win combo.
 
 #instructions()
 tic_tac_toe()
