856638058301f501a06d73056dfd843bf786a065
Puff-yay <demonic.puff@gmail.com>
2025-12-11 15:30:05 -0500

    Code Overhaul
    I think this is maybe the final update until I learn how to make a better win condition that I can actually understand.

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -15,21 +15,86 @@ def instructions():
 
 def tic_tac_toe():
     board = starting_gameboard()
-    player = True #True for X, False for Y
+    player = True #True for X, False for O
     player_num = 1
     turn = 0
-    win = False
+    win = "No"
     pos = 0
-    while win != True:
+    while win != "Yes":
         print(board[0])
         print(board[1])
         print(board[2])
+        if turn == 9:
+            print("This is a tied game.")
+            return
+        if board[0][0] == board[0][1] == board[0][2] and board[0][0] != " ": #Top Row
+            print("We have a winner.")
+            if board[0][0] == "X":
+                print("Player 1 has won.")
+                return
+            else:
+                print("Player 2 has won.")
+                return
+        if board[1][0] == board[1][1] == board[1][2] and board[1][0] != " ": #Center Row
+            print("We have a winner.")
+            if board[1][0] == "X":
+                print("Player 1 has won.")
+                return
+            else:
+                print("Player 2 has won.")
+                return
+        if board[2][0] == board[2][1] == board[2][2] and board[2][0] != " ": #Bottom Row
+            print("We have a winner.")
+            if board[2][0] == "X":
+                print("Player 1 has won.")
+                return
+            else:
+                print("Player 2 has won.")
+                return
+        if board[0][0] == board[1][0] == board[2][0] and board[0][0] != " ": #Left Column
+            print("We have a winner.")
+            if board[0][0] == "X":
+                print("Player 1 has won.")
+                return
+            else:
+                print("Player 2 has won.")
+                return
+        if board[0][1] == board[1][1] == board[2][1] and board[0][1] != " ": #Middle Column
+            print("We have a winner.")
+            if board[0][1] == "X":
+                print("Player 1 has won.")
+                return
+            else:
+                print("Player 2 has won.")
+                return
+        if board[0][2] == board[1][2] == board[2][2] and board[0][2] != " ": #Right Column
+            print("We have a winner.")
+            if board[0][2] == "X":
+                print("Player 1 has won.")
+                return
+            else:
+                print("Player 2 has won.")
+                return
+        if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ": #Top Left to Bottom Right Diagonal
+            print("We have a winner.")
+            if board[0][0] == "X":
+                print("Player 1 has won.")
+                return
+            else:
+                print("Player 2 has won.")
+                return
+        if board[2][0] == board[1][1] == board[0][2] and board[2][0] != " ":
+            print("We have a winner.")
+            if board[2][0] == "X":
+                print("Player 1 has won.")
+                return
+            else:
+                print("Player 2 has won.")
+                return
         pos = input(f"You are player {player_num}. Where would you like to play?\n")
         pos = int(pos) - 1
         row = pos // 3
         col = pos % 3
-        print("Calculating...")
-        time.sleep(0.5)
         if pos < 0 or pos > 9:
             print("Please pick another square")
             continue
@@ -44,20 +109,11 @@ def tic_tac_toe():
                 player_num = 2
                 turn += 1
             else:
-                board[row][col] = "Y"
+                board[row][col] = "O"
                 player = not player
                 player_num = 1
                 turn += 1
-        if turn == 9:
-            print("This is a tied game.")
-            win = "Draw"
-    print("You have drawn this game. Thank you for playing.")
 
-def win_check():
-    winning_combos = [[0,0],[1,1],[2,2]],[[0,0],[1,0],[2,0]],[[0,0],[0,1],[0,2]],[[2,0],[2,1],[2,2]],[[1,0],[1,1],[1,2]],[[2,0],[1,1],[0,2]],[[0,1],[1,1],[2,1]],[[0,2],[1,2],[2,2]]
-
-
-#Make a win condition that loop through every win combo.
 
 #instructions()
 tic_tac_toe()
